6f620c3589d421d19adb896c4631e8d76e3726b5
nvictor-netapp <121135127+nvictor-netapp@users.noreply.github.com>
2022-12-23 17:14:46 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,24 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "web" {
-  ami           = "ami-a1b2c3d4"
-  instance_type = "t3.nano"
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
