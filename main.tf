bb1b8355f847364756bbbcf8f18e369c2e30db51
nvictor-netapp <121135127+nvictor-netapp@users.noreply.github.com>
2022-12-23 17:09:09 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,4 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
-}
-
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
+  ami           = "ami-a1b2c3d4"
   instance_type = "t3.nano"
 }
