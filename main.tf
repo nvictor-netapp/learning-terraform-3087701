12688b0fcefe760b34db713bed17c010385a8c96
nvictor-netapp <121135127+nvictor-netapp@users.noreply.github.com>
2022-12-23 17:36:23 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,9 +16,9 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "var.instance_type"
+  instance_type = var.instance_type
 
   tags = {
-    Name = "HelloWorld"
+    Name = "My first TF"
   }
 }
