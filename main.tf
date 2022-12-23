9f5300e63ba735547d94b471949f8ee697850504
nvictor-netapp <121135127+nvictor-netapp@users.noreply.github.com>
2022-12-23 16:40:16 -0500

    removed data block

--- main.tf
+++ main.tf
@@ -1,18 +1,4 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ami-08c1d360ff0d39882"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
 resource "aws_instance" "Ubuntu-22-04" {
-  ami           = data.aws_ami.app_ami.id
+  ami           = "ami-08c1d360ff0d39882"
   instance_type = var.instance_type
 }
